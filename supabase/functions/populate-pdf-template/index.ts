cb602402427619c2cdf69a8df369f8b075966937
Van <wanniejvr08@gmail.com>
2024-02-18 22:21:22 +0200

    init(console): log structure init

--- supabase/functions/populate-pdf-template/index.ts
+++ supabase/functions/populate-pdf-template/index.ts
@@ -2,18 +2,34 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-console.log("Hello from Functions!")
-
 Deno.serve(async (req) => {
-  const { name } = await req.json()
+  // Create console log group - collapsed
+  const currentDate = new Date(); // Get the date
+  const formattedDate = currentDate.toISOString(); // Format the date as a string
+  console.groupCollapsed(`[INFO] Function invoked - Populating pdf template - ${formattedDate}`);
+
+  // Collect request
+  const reqJson = await req.json()
+  // Log request
+  console.groupCollapsed("Request");
+  console.table(reqJson);
+  console.groupEnd();
+  // COllect vars from request
+  const { name } = reqJson
+  
+  // Do something
   const data = {
     message: `Hello ${name}!`,
   }
 
+  // Close console log group
+  console.groupEnd();
+  
+  // Return response
   return new Response(
     JSON.stringify(data),
     { headers: { "Content-Type": "application/json" } },
-  )
+    )
 })
 
 /* To invoke locally:
